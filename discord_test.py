5387aab0b126be718f89b3a011dcb38da9fe4418
Schmaew <109778280+Schmaew@users.noreply.github.com>
2024-02-09 14:34:10 +0700

    Update discord_test.py

--- discord_test.py
+++ discord_test.py
@@ -5,11 +5,11 @@ import requests
 payload = {"content": "/play รู้กันแค่สี้"}
 
 header = {
-    "authorization": "NjI1NjQxNTE1NzA4MDU1NTUy.GFpfhK.GOZG-i5REp7w0U7VKq4itgdwA-wWhdVgOxrq2E"
+    ""
 }
 
 headeral = {
-    "authorization": "NjI0ODY1NTA5MTQ5NzY5Nzcw.GLFsNi.bmt72uID-HbqGJL3egirrWifII5_Yfn8YgjbEA"
+    ""
 }
 
 r = requests.post(
